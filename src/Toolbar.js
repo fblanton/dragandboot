a6bce6204d8dc1467c6c4b613a1d835a4cf0de8d
Forrest Blanton <fblanton@gmail.com>
2016-09-30 13:20:10 -0700

    The user can create a landing page. #1

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,19 +1,30 @@
 const React = require('react');
-const { createDraggable } = require('./drag-and-drop');
+const { createDraggable, createDroppable } = require('./drag-and-drop');
 
-const Draggable = createDraggable('toolbar');
+const Draggable = createDraggable('toolbar-item');
+const Droppable = createDroppable('toolbar-item');
+const DragAndDrop = ({ children }) =>
+(  <Draggable>
+    <Droppable>
+      { [...React.Children.toArray(children)] }
+    </Droppable>
+  </Draggable>
+);
 
 const Toolbar = () =>
   <div>
-    <Draggable
-      style={{ display: 'inline-block' }}
-      
-    >
+    <Draggable data-name='drag1' style={{ display: 'inline-block' }}>
       <span>First Item</span>
     </Draggable>
     <Draggable style={{ display: 'inline-block' }}>
       <span>My Toolbar Goes Here</span>
     </Draggable>
+    <Droppable className='drop'>
+      <div>Drop Here</div>
+    </Droppable>
+    <DragAndDrop className='drop2'>
+      <div>Drop Here</div>
+    </DragAndDrop>
   </div>;
 
 module.exports = Toolbar;
--- src/drag-and-drop.js
+++ src/drag-and-drop.js
@@ -1,29 +1,64 @@
 const React = require('react');
-const { DragSource } = require('react-dnd');
+const { DragSource, DropTarget } = require('react-dnd');
 
-const defaultDragSource = { beginDrag: () => ({ text: 'Hello Dragging' }) };
+// create a draggable react compnent
+const defaultDragSpec = {
+  beginDrag: () => ({ text: 'Hello Dragging' }),
+  endDrag: (props, monitor) => {
+    if (monitor.didDrop()) console.log(props, monitor.getDropResult());
+  }
+};
 
-const collect = (connect, monitor) => ({
+const defaultDragCollect = (connect, monitor) => ({
   connectDragSource: connect.dragSource(),
   isDragging: monitor.isDragging()
 });
 
 const Draggable = React.createClass({
-  render: function () {
+  render: function() {
     const { connectDragSource, isDragging, children, style } = this.props;
 
-    return connectDragSource((
+    return connectDragSource(
       <div
         data-dragging={ isDragging }
-        className={ isDragging ? 'draggable dragging' : 'draggable not-dragging' }
+        className={ isDragging ? 'draggable dragging' : 'draggable' }
         style={{ ...style, opacity: (isDragging ? 0.5 : 1) }}
       >
         { [...React.Children.toArray(children)] }
       </div>
-    ));
+    );
+  }
+});
+
+// create a react component that can receive draggable items
+const defaultDropSpec = {
+  drop: (props, monitor, component) => {
+    return {dropProps: props};
+  }
+};
+
+const defaultDropCollect = (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  isOver: monitor.isOver()
+});
+
+const Droppable = React.createClass({
+  render: function() {
+    const { connectDropTarget, isOver, children, style } = this.props;
+
+    return connectDropTarget(
+      <div
+        data-hover={ isOver }
+        className= { isOver ? 'hoverable hovered' : 'hoverable'}
+        style={{ ...style }}
+      >
+        { [...React.Children.toArray(children)] }
+      </div>
+    );
   }
 });
 
 module.exports = {
-  createDraggable: (type = 'draggable', source = defaultDragSource) => DragSource(type, source, collect)(Draggable)
+  createDraggable: (type = 'drag-and-drop', spec = defaultDragSpec, collect = defaultDragCollect) => DragSource(type, spec, collect)(Draggable),
+  createDroppable: (types = 'drag-and-drop', spec = defaultDropSpec, collect = defaultDropCollect) => DropTarget(types, spec, collect)(Droppable)
 };
