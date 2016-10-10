0415f2f84b40cf717930a246bc0060222fa11877
Forrest Blanton <fblanton@gmail.com>
2016-10-10 10:19:35 -0700

    The user can edit elements on the page. #6

--- src/forms/index.js
+++ src/forms/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   ColForm: require('./ColForm'),
   h1Form: require('./h1Form'),
+  pForm: require('./pForm'),
   JumbotronForm: require('./JumbotronForm')
 }
--- src/forms/pForm.js
+++ src/forms/pForm.js
@@ -3,30 +3,31 @@ const { Field, reduxForm } = require('redux-form');
 const { connect } = require('react-redux');
 const _ = require('underscore');
 const { Form, FormGroup, Label, Input, Button, ButtonGroup, ButtonToolbar } = require('reactstrap');
-
 const liveUpdate = (value, id, dispatch) => {
   dispatch({type: 'UPDATE_COMPONENT', id, update: {children: [value]}});
   return value;
 }
-const HeaderForm = ({ reset, onChangeAction, handleSubmit, dispatch, component: { id } }) =>
+
+const PInput = props =>
+  <div>
+    <Input type='textarea' {...props.input} style={{width: '100%', height: '7rem'}} />
+  </div>;
+
+const PForm = ({ reset, onChangeAction, handleSubmit, dispatch, component: { id } }) =>
 <Form onSubmit={ handleSubmit }>
-  <h1>Header</h1>
+  <h1>Paragraph</h1>
   <hr />
   <FormGroup>
-    select box here
-  </FormGroup>
-  <FormGroup>
     <Field
-      name='header'
-      component='input'
-      type='text'
+      name='paragraph'
+      component={ PInput }
       normalize={ value => liveUpdate(value, id, dispatch) }
       id={ id }
     />
   </FormGroup>
   <ButtonToolbar>
     <ButtonGroup>
-      <Button color="primary" type='submit' onClick= { handleSubmit((newHeader) => formSubmit(newHeader, id, dispatch)) }>Apply</Button>
+      <Button color="primary" type='submit' onClick= { handleSubmit(updated => formSubmit(updated, id, dispatch)) }>Apply</Button>
     </ButtonGroup>
     <ButtonGroup>
       <Button color="warning" type='button' onClick= { reset }>Reset</Button>
@@ -34,11 +35,11 @@ const HeaderForm = ({ reset, onChangeAction, handleSubmit, dispatch, component: 
   </ButtonToolbar>
 </Form>
 
-const formSubmit = ({ header }, id, dispatch) => {
-  dispatch({type: 'UPDATE_COMPONENT', id, update: {children: [header]}});
+const formSubmit = ({ paragraph }, id, dispatch) => {
+  dispatch({type: 'UPDATE_COMPONENT', id, update: {children: [paragraph]}});
   dispatch({type: 'EDIT_COMPONENT', id: ''});
 }
 
 module.exports = connect(
-  ({ editComponent: id, components: c }) => ({ initialValues: { header: c[id].children[0] } })
-)(reduxForm({form: 'HeaderForm'})(HeaderForm));
+  ({ editComponent: id, components: c }) => ({ initialValues: { paragraph: c[id].children[0] } })
+)(reduxForm({form: 'ParagraphForm'})(PForm));
